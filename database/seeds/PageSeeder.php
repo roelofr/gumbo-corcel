bf923e3962d1190692dd43ac2ef5987f433fd2a0
Roelof Roos <github@roelof.io>
2020-12-29 17:57:39 +0100

    Improved Page seeder to make dummy pages locally

--- database/seeds/PageSeeder.php
+++ database/seeds/PageSeeder.php
@@ -3,7 +3,9 @@
 declare(strict_types=1);
 
 use App\Models\Page;
+use Faker\Generator;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\App;
 
 class PageSeeder extends Seeder
 {
@@ -11,13 +13,22 @@ class PageSeeder extends Seeder
      * Run the database seeds.
      * @return void
      */
-    public function run()
+    public function run(Generator $faker)
     {
         foreach (Page::getRequiredPages() as $slug => $title) {
             Page::updateOrCreate(
                 ['slug' => $slug, 'group' => null],
                 ['title' => $title, 'type' => Page::TYPE_REQUIRED]
             );
+        }
+
+        if (App::environment(['local'])) {
+            $categories = array_merge([null], $faker->words(3));
+            foreach ($categories as $category) {
+                factory(Page::class, $faker->numberBetween(1, 20))->create([
+                    'group' => $category
+                ]);
+            }
         }
     }
 }
